31cd6e470cc1692013a94b9187719288871dfc36
Morgan Perry <morganperry@posteo.net>
2024-11-21 13:44:11 +1000

    Keymaps need to be silent otherwise noice intercepts them

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -14,15 +14,18 @@
       action = "<C-W>";
       key = "<leader>w";
       mode = "n";
+      options.silent = true;
     }
     {
       action = "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>";
       key = "<leader>u";
       mode = "n";
+      options.silent = true;
     }
     {
       action = ":w<CR>";
       key = "<C-x><C-s>";
+      options.silent = true;
     }
   ];
 }
