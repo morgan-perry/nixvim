d468d972f08f0e1af456a1cd2de7df46c2b2ec1d
Morgan Perry <morganperry@posteo.net>
2025-01-06 23:09:37 +1000

    Competitest with some keybinds implemented

--- config/competitest.nix
+++ config/competitest.nix
@@ -1,4 +1,9 @@
 {
+  config,
+  lib,
+  ...
+}:
+{
   plugins.competitest = {
     enable = true;
     settings = {
@@ -21,8 +26,55 @@
         };
       };
       evaluate_template_modifiers = true;
-      received_problems_path = "$(HOME)/doc/cp/$(JUDGE)/$(CONTEST)/$(PROBLEM)/main.$(FEXT)";
-      template_file = "$(HOME)/doc/cp/templates/template.$(FEXT)";
+      received_problems_path = "$(HOME)/doc/pgrm/cp/$(JUDGE)/$(CONTEST)/$(PROBLEM)/main.$(FEXT)";
+      template_file = "$(HOME)/doc/pgrm/cp/templates/template.$(FEXT)";
     };
   };
+  keymaps = lib.optionals config.plugins.competitest.enable [
+    {
+      mode = "n";
+      key = "<leader>pr";
+      action = ":CompetiTest run<CR>";
+      options = {
+        desc = "Comp run";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>pe";
+      action = ":CompetiTest edit_testcase<CR>";
+      options = {
+        desc = "Comp edit testcase";
+        # silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>pa";
+      action = ":CompetiTest add_testcase<CR>";
+      options = {
+        desc = "Comp add testcase";
+        # silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>pp";
+      action = ":CompetiTest receive problem<CR>";
+      options = {
+        desc = "Comp delete testcase";
+        # silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>pd";
+      action = ":CompetiTest delete_testcase<CR>";
+      options = {
+        desc = "Comp delete testcase";
+        # silent = true;
+      };
+    }
+  ];
 }
