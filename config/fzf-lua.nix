ccab7a59046cf0418eae890dba7ad9ff3d25cb69
Morgan Perry <morganperry@posteo.net>
2025-01-06 23:09:18 +1000

    lsp_references keybinding

--- config/fzf-lua.nix
+++ config/fzf-lua.nix
@@ -10,6 +10,7 @@
       "<leader>ps" = "lsp_workspace_symbols";
       "<leader>si" = "lsp_document_symbols";
       "<leader>ca" = "lsp_code_actions";
+      "gr" = "lsp_references";
       "<leader>b" = "buffers";
     };
   };
